13b7935a17d271512c81594f64d134719fe8560b
Arvin Fernandez <arvinf07@gmail.com>
2021-02-12 12:24:24 -0500

    commit tree 717181b4b146431f9923f1416a84b764826e5f68

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+function findMatching(drivers, string){
+  const matches = drivers.filter((driver) => {
+    return driver.toUpperCase() === string.toUpperCase()
+  })
+  return matches
+}
+
+function fuzzyMatch(drivers, string){
+  const matches = drivers.filter((driver) => {
+   return driver.startsWith(string, 0);
+  })
+  return matches
+}
+
+function matchName(drivers, string){
+  const matches = drivers.filter((driver) => {
+    return driver['name'] === string
+  })
+  return matches
+
+}
